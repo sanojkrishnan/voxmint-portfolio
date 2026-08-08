b99b40e32966477bed5cfacd050c9c2273986fa2
sanoj krishnan m <sanojkrishnan12@gmail.com>
2026-08-09 01:31:45 +0530

    date 9/8/2026

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 
 function Header() {
   return (
-    <div className="bg-gray-100 dark:bg-black flex p-4 py-0 items-center justify-between">
+    <div className="bg-gray-100 dark:bg-gray-900 flex p-4 py-4 items-center justify-between">
       <LogoTypeOne size={30} animate={false} color="#3D5AA8" />
       <ThemeToggle />
     </div>
